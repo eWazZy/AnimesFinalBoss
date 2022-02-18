4050a5431c3b6bd63b05273cbfd7471339862d03
João <juaum192001@gmail.com>
2022-02-18 00:53:26 -0300

    second commit

---
+++ AnimesFinalBoss/Controller/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace AnimesFinalBoss
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
